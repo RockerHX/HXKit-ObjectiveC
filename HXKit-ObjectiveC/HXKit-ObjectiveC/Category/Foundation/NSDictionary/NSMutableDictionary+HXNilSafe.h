218dbee8d534c7f1534a62657592b85d0042a459
RockerHX <RockerHX@gmail.com>
2016-10-07 18:47:48 +0800

    Add Category Of NSDictionary+HXNilSafe And NSMutableDictionary+HXNilSafe
    For NSDictionary and NSMutableDictionary can accept nil that don’t
    throw error.

---
+++ HXKit-ObjectiveC/HXKit-ObjectiveC/Category/Foundation/NSDictionary/NSMutableDictionary+HXNilSafe.h
@@ -0,0 +1,14 @@
+//
+//  NSMutableDictionary+HXNilSafe.h
+//  HXKit-ObjectiveC
+//
+//  Created by ShiCang on 16/9/28.
+//  Copyright © 2016年 Cave. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSMutableDictionary (HXNilSafe)
+
+@end
