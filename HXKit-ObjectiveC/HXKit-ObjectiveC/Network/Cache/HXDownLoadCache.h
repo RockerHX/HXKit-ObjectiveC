d479988a5def09c71a5747520ed37c094708e080
RockerHX <RockerHX@gmail.com>
2016-10-10 22:13:47 +0800

    Add HXDownLoadCache
    For networking down load cache.

---
+++ HXKit-ObjectiveC/HXKit-ObjectiveC/Network/Cache/HXDownLoadCache.h
@@ -0,0 +1,38 @@
+//
+//  HXDownLoadCache.h
+//  HXKit-ObjectiveC
+//
+//  Created by miaios on 16/5/12.
+//  Copyright © 2016年 Cave. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+@interface HXDownLoadCache : NSObject
+
+@property (nonatomic, weak, readonly) NSURLCache *cache;
+
++ (instancetype)cache;
++ (instancetype)cacheWithCache:(NSURLCache *)cache;
+
+/**
+ *  Timeout     : 10.0f
+ *  CachePolicy : NSURLRequestReturnCacheDataElseLoad
+ */
+- (NSURLSessionDownloadTask *)downLoadWithURL:(NSURL *)url completionHandler:(void (^)(HXDownLoadCache *cache, NSData *data, NSURLResponse *response, NSError *error))completionHandler;
+
+/**
+ *  Custom Request
+ */
+- (NSURLSessionDownloadTask *)downLoadWithRequest:(NSURLRequest *)request completionHandler:(void (^)(HXDownLoadCache *cache, NSData *data, NSURLResponse *response, NSError *error))completionHandler;
+- (NSURLSessionDownloadTask *)downLoadWithURL:(NSURL *)url configuration:(NSURLSessionConfiguration *)configuration completionHandler:(void (^)(HXDownLoadCache *cache, NSData *data, NSURLResponse *response, NSError *error))completionHandler;
+- (NSURLSessionDownloadTask *)downLoadWithRequest:(NSURLRequest *)request configuration:(NSURLSessionConfiguration *)configuration completionHandler:(void (^)(HXDownLoadCache *cache, NSData *data, NSURLResponse *response, NSError *error))completionHandler;
+
+@end
+
+
+NS_ASSUME_NONNULL_END
