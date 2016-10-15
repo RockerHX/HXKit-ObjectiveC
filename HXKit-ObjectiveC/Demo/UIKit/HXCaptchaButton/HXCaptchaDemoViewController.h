f4d5f4bb29dad890ef7cd1f9dd226f07ab9c7f59
RockerHX <RockerHX@gmail.com>
2016-10-15 22:17:53 +0800

    Add[Code]: HXCaptchaButton Demo Code

---
+++ HXKit-ObjectiveC/Demo/UIKit/HXCaptchaButton/HXCaptchaDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXCaptchaDemoViewController.h
+//  HXKit-ObjectiveC
+//
+//  Created by ShiCang on 16/10/15.
+//  Copyright © 2016年 Cave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXCaptchaDemoViewController : UIViewController
+
+@end
---
+++ HXKit-ObjectiveC/Demo/UIKit/HXCaptchaButton/HXCaptchaDemoViewController.m
@@ -0,0 +1,37 @@
+//
+//  HXCaptchaDemoViewController.m
+//  HXKit-ObjectiveC
+//
+//  Created by ShiCang on 16/10/15.
+//  Copyright © 2016年 Cave. All rights reserved.
+//
+
+#import "HXCaptchaDemoViewController.h"
+
+@interface HXCaptchaDemoViewController ()
+
+@end
+
+@implementation HXCaptchaDemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
